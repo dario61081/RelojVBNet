42300e58a9393da7ba1f92a46c622cdbb242727b
Dario Garcia <Dario>
2024-12-02 15:27:15 -0300

    Refactoring a utiles

--- Form1.vb
+++ Form1.vb
@@ -5,254 +5,237 @@ Imports RelojVBNET.Models
 
 Public Class Form1
 
-	Public WithEvents Device As ZKBiometricDevice
+    Public WithEvents Device As ZKBiometricDevice
 
+    Private Sub Conectar(ipaddress As String)
 
+        Me.Cursor = Cursors.WaitCursor
 
+        If Device Is Nothing Then
+            log("Iniciando dispositivo", True)
+            Device = New ZKBiometricDevice()
+        End If
 
-	Private Sub Conectar(ipaddress As String)
+        If Device.Connected Then
+            log("(!) ya esta conectado", True)
+            Return
+        End If
 
-		Me.Cursor = Cursors.WaitCursor
 
-		If Device Is Nothing Then
-			log("Iniciando dispositivo", True)
-			Device = New ZKBiometricDevice()
-		End If
+        Dim estado = Device.Connect(ipaddress, 4370)
+        If estado Then
+            log("(ok) dispositivo conectado", True)
+        ElseIf Not estado Then
+            log("(fail) dispositivo no encontrado", True)
+        End If
 
-		If Device.Connected Then
-			log("(!) ya esta conectado", True)
-			Return
-		End If
+        Me.Cursor = Cursors.Default
+    End Sub
 
+    Private Function LeerLogs() As List(Of AttendanceRecord)
 
-		Dim estado = Device.Connect(ipaddress, 4370)
-		If estado Then
-			log("(ok) dispositivo conectado", True)
-		ElseIf Not estado Then
-			log("(fail) dispositivo no encontrado", True)
-		End If
 
-		Me.Cursor = Cursors.Default
 
+        If Device Is Nothing OrElse Not Device.Connected Then
 
-	End Sub
+            Return Nothing
+        End If
 
-	Private Function LeerLogs() As List(Of AttendanceRecord)
+        Dim listado = Device.GetAttendanceLogs()
 
+        Return listado
 
 
-		If Device Is Nothing OrElse Not Device.Connected Then
 
-			Return Nothing
-		End If
 
-		Dim listado = Device.GetAttendanceLogs()
+    End Function
+    Private Sub Desconectar()
 
-		Return listado
+        If Device Is Nothing Then
+            log("(!) dispositivo no iniciado", True)
+            Return
+        End If
 
+        If Device.Connected Then
+            log("(OK) dispositivo desconectado", True)
+            Device.Disconnect()
+        End If
 
+        log("(system) liberado asignacion de memoria", True)
+        Device = Nothing
 
+    End Sub
 
-	End Function
-	Private Sub Desconectar()
-		If Device Is Nothing Then
-			log("(!) dispositivo no iniciado", True)
-			Return
-		End If
 
-		If Device.Connected Then
-			log("(OK) dispositivo desconectado", True)
-			Device.Disconnect()
-		End If
+    Public Sub log(message As String, Optional allowMessage As Boolean = False)
 
-		log("(system) liberado asignacion de memoria", True)
-		Device = Nothing
+        Dim item As New ListViewItem With {
+            .Text = $"{DateTime.Now().GetDateTimeFormats()(0)}"
+        }
+        item.SubItems.Add(message)
+        lvEventos.Items.Add(item)
 
+        If lvEventos.Items.Count > 0 Then
+            lvEventos.Items(lvEventos.Items.Count - 1).EnsureVisible()
+        End If
 
+        If allowMessage Then
+            With NotifyIcon1
+                .BalloonTipTitle = "Mensaje"
+                .BalloonTipText = message
+                .BalloonTipIcon = ToolTipIcon.Info ' Opciones: Info, Warning, Error, None
+                .ShowBalloonTip(5000)
+            End With
+        End If
 
-	End Sub
 
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        NotifyIcon1.Visible = True
+        log("Iniciando", True)
 
-	Public Sub log(message As String, Optional allowMessage As Boolean = False)
+        ' objectos de prueba
+        Dim lista_dispositivos = New List(Of DispositivoModel) From {
+            New DispositivoModel With {
+                .Descripcion = "Local",
+                .DireccionIp = "192.168.0.201"
+            }
+        }
 
-		Dim item As New ListViewItem With {
-			.Text = $"{DateTime.Now().GetDateTimeFormats()(0)}"
-		}
-		item.SubItems.Add(message)
-		lvEventos.Items.Add(item)
+        ' lista de relojes
+        For Each row As DispositivoModel In lista_dispositivos
+            Relojes.AddItem(row)
+        Next
 
-		If lvEventos.Items.Count > 0 Then
-			lvEventos.Items(lvEventos.Items.Count - 1).EnsureVisible()
-		End If
+    End Sub
 
-		If allowMessage Then
-			With NotifyIcon1
-				.BalloonTipTitle = "Mensaje"
-				.BalloonTipText = message
-				.BalloonTipIcon = ToolTipIcon.Info ' Opciones: Info, Warning, Error, None
-				.ShowBalloonTip(5000)
-			End With
-		End If
 
 
-	End Sub
-	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-		NotifyIcon1.Visible = True
-		log("Iniciando", True)
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        If Device Is Nothing Then
+            log("(fail) dispositivo no ha sido conectado", True)
+            Return
+        End If
 
-		' objectos de prueba
-		Dim lista_dispositivos = New List(Of DispositivoModel) From {
-			New DispositivoModel With {
-				.Descripcion = "Local",
-				.DireccionIp = "192.168.0.201"
-			}
-		}
+        If Device.Connected Then
+            Device.Disconnect()
+            log("(ok) dispositivo desconectado", True)
+        End If
+    End Sub
 
-		' lista de relojes
-		For Each row As DispositivoModel In lista_dispositivos
-			Relojes.AddItem(row)
-		Next
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
 
+        Dim Uestado = Device.ClearLogs
+        If Uestado Then
+            log("(OK) logs limpiados")
+        ElseIf Not Uestado Then
+            log("(Fail) no se ha ejecutado la limpieza de logs")
+        End If
 
+    End Sub
 
+    Private Sub Device_OnFinger() Handles Device.OnFinger
+        log("Dedo")
+    End Sub
 
-	End Sub
+    Private Sub Device_OnKeyPress() Handles Device.OnKeyPress
+        log("Teclado")
+    End Sub
 
-	Private Sub Button1_Click(sender As Object, e As EventArgs)
-		Conectar(txtIpAddress.Text)
-	End Sub
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
 
-	Private Sub Button2_Click(sender As Object, e As EventArgs)
+        ' Device.Disconnect()
 
 
+    End Sub
 
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
 
+    End Sub
 
-	End Sub
+    Private Sub ActualizarListaToolStripMenuItem_Click(sender As Object, e As EventArgs)
 
-	Private Sub Button3_Click(sender As Object, e As EventArgs)
-		If Device Is Nothing Then
-			log("(fail) dispositivo no ha sido conectado", True)
-			Return
-		End If
+        Relojes.RefreshList()
+        log("(ok) Registro de relojes actualizado", True)
+    End Sub
 
-		If Device.Connected Then
-			Device.Disconnect()
-			log("(ok) dispositivo desconectado", True)
-		End If
-	End Sub
+    Private Sub Relojes_OnSelectedItem(record As DispositivoModel) Handles Relojes.OnSelectedItem
 
-	Private Sub Button4_Click(sender As Object, e As EventArgs)
+        If record Is Nothing Then
+            log("No hay seleccionados")
+            Return
+        End If
 
-		Dim Uestado = Device.ClearLogs
-		If Uestado Then
-			log("(OK) logs limpiados")
-		ElseIf Not Uestado Then
-			log("(Fail) no se ha ejecutado la limpieza de logs")
-		End If
+        log($"(selected) {record.Descripcion }")
 
-	End Sub
+        FichaReloj1.MostrarFicha(record)
 
-	Private Sub Device_OnFinger() Handles Device.OnFinger
-		log("Dedo")
-	End Sub
 
-	Private Sub Device_OnKeyPress() Handles Device.OnKeyPress
-		log("Teclado")
-	End Sub
+    End Sub
 
-	Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lvEventos.SelectedIndexChanged
 
-		' Device.Disconnect()
+    End Sub
 
+    Private Sub Relojes_OnReadLogs(record As DispositivoModel) Handles Relojes.OnReadLogs
 
-	End Sub
 
-	Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+        Dim lista_registro As List(Of AttendanceRecord) = Nothing
+        Dim thread As New Thread(Sub() lista_registro = LeerLogs())
+        ' actualizar el indice
+        TabControl1.SelectedTab = TabPage2
 
-	End Sub
+        log("(espere) leyendo datos del reloj...", True)
+        thread.Start()
+        thread.Join()
 
-	Private Sub ActualizarListaToolStripMenuItem_Click(sender As Object, e As EventArgs)
+        lvLog.Items.Clear()
+        For Each row In lista_registro
+            log($"{row.DateTime } {row.EnrollNumber } {row.InOutMode }")
 
-		Relojes.RefreshList()
-		log("(ok) Registro de relojes actualizado", True)
-	End Sub
+            Dim nitem As New ListViewItem With {
+                .Text = row.DateTime
+            }
+            nitem.SubItems.Add(row.VerifyMode)
+            nitem.SubItems.Add(row.InOutMode)
+            nitem.SubItems.Add(row.EnrollNumber)
 
-	Private Sub Relojes_OnSelectedItem(record As DispositivoModel) Handles Relojes.OnSelectedItem
+            lvLog.Items.Add(nitem)
 
-		If record Is Nothing Then
-			log("No hay seleccionados")
-			Return
-		End If
+        Next
 
-		log($"(selected) {record.Descripcion }")
+        'exportar los datos
+        Dim base_filename As String = Utiles.GetTempFilename()
+        Dim dumps_directory = Utiles.GetDirectory("dumps")
+        Dim save_filename As String = Path.Combine(dumps_directory, $"db_{record.DireccionIp}_{base_filename}")
 
-		FichaReloj1.MostrarFicha(record)
+        ObjectReaderWriter(Of AttendanceRecord).SaveToJson(lista_registro, save_filename)
 
+        log("(OK) Lectura finalizada", True)
+    End Sub
 
-	End Sub
+    Private Sub Relojes_OnDisconnecting(record As DispositivoModel) Handles Relojes.OnDisconnecting
+        log($"Desconectar reloj {record.DireccionIp }")
+        Desconectar()
+    End Sub
 
-	Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lvEventos.SelectedIndexChanged
+    Private Sub Relojes_OnClearlogs(record As DispositivoModel) Handles Relojes.OnClearlogs
+        log($"Limpiando todos los logs de {record.DireccionIp }")
 
-	End Sub
+        lvLog.Items.Clear()
+    End Sub
 
-	Private Sub Relojes_OnReadLogs(record As DispositivoModel) Handles Relojes.OnReadLogs
+    Private Sub Relojes_OnConnecting(record As DispositivoModel) Handles Relojes.OnConnecting
+        log($"iniciar Conexion a {record.DireccionIp }")
+        Conectar(record.DireccionIp)
+        Relojes.ListaBloqueada = True
 
+    End Sub
 
-		Dim lista_registro As List(Of AttendanceRecord) = Nothing
-		Dim thread As New Thread(Sub() lista_registro = LeerLogs())
-		' actualizar el indice
-		TabControl1.SelectedTab = TabPage2
 
-		log("(espere) leyendo datos del reloj...", True)
-		thread.Start()
-		thread.Join()
 
-		lvLog.Items.Clear()
-		For Each row In lista_registro
-			log($"{row.DateTime } {row.EnrollNumber } {row.InOutMode }")
+    Private Sub CerrarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CerrarToolStripMenuItem.Click
+        Close()
 
-			Dim nitem As New ListViewItem With {
-				.Text = row.DateTime
-			}
-			nitem.SubItems.Add(row.VerifyMode)
-			nitem.SubItems.Add(row.InOutMode)
-			nitem.SubItems.Add(row.EnrollNumber)
-
-			lvLog.Items.Add(nitem)
-
-		Next
-
-
-		Dim timestamp As DateTime = DateTime.UtcNow
-		Dim tag As String = timestamp.ToString("yyyy-mm-dd hhmmss")
-		Dim dump_directory = Path.Combine(Directory.GetCurrentDirectory, "dumps")
-		If Not Directory.Exists(dump_directory) Then
-			Directory.CreateDirectory(dump_directory)
-		End If
-
-		Dim save_filename As String = Path.Combine(dump_directory, $"db_{record.DireccionIp }_{tag}.json")
-
-		ObjectReaderWriter(Of AttendanceRecord).SaveToJson(lista_registro, save_filename)
-
-		log("(OK) Lectura finalizada", True)
-	End Sub
-
-	Private Sub Relojes_OnDisconnecting(record As DispositivoModel) Handles Relojes.OnDisconnecting
-		log($"Desconectar reloj {record.DireccionIp }")
-		Desconectar()
-	End Sub
-
-	Private Sub Relojes_OnClearlogs(record As DispositivoModel) Handles Relojes.OnClearlogs
-		log($"Limpiando todos los logs de {record.DireccionIp }")
-
-		lvLog.Items.Clear()
-	End Sub
-
-	Private Sub Relojes_OnConnecting(record As DispositivoModel) Handles Relojes.OnConnecting
-		log($"iniciar Conexion a {record.DireccionIp }")
-		Conectar(record.DireccionIp)
-		Relojes.ListaBloqueada = True
-
-	End Sub
+    End Sub
 End Class
---
+++ classes/Utiles.vb
@@ -0,0 +1,25 @@
+﻿Imports System.IO
+
+Module Utiles
+
+
+    'Genera un nombre de archivo temporal
+    Public Function GetTempFilename(Optional extension As String = ".json") As String
+        Dim timestamp As DateTime = DateTime.UtcNow
+        Return timestamp.ToString("yyyy_MM_dd_hhmmss") + extension
+    End Function
+
+    'Retorna el directorio de trabajo
+    Public Function GetDirectory(Optional folder As String = "data") As String
+
+        Dim source = Path.Combine(Directory.GetCurrentDirectory, folder)
+        If Not Directory.Exists(source) Then
+            Directory.CreateDirectory(source)
+        End If
+
+        Return source
+
+    End Function
+
+
+End Module

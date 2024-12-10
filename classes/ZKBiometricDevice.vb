3f1800655af1430c3e7db15f9dbe53ed1e4dc68a
Dario Garcia <Dario>
2024-12-10 13:09:33 -0300

    refactoring

--- classes/ZKBiometricDevice.vb
+++ classes/ZKBiometricDevice.vb
@@ -9,6 +9,7 @@ Public Class ZKBiometricDevice
 
     Private IsConnected As Boolean
     Private DeviceNumber As Integer
+    Public SerialNumber As String
 
     Public Sub New()
         Zkem = New CZKEM()
@@ -27,6 +28,8 @@ Public Class ZKBiometricDevice
     Public Function Connect(Dispositivo As DispositivoModel) As Boolean
         DeviceNumber = Dispositivo.IdDispositivo
         If Zkem.Connect_Net(Dispositivo.DireccionIp, Dispositivo.Puerto) Then
+            Zkem.GetSerialNumber(DeviceNumber, SerialNumber)
+
             IsConnected = True
             ' Establecer la máquina en modo normal
             Zkem.EnableDevice(DeviceNumber, True)
@@ -53,38 +56,6 @@ Public Class ZKBiometricDevice
     ''' Obtiene los registros de asistencia del dispositivo.
     ''' </summary>
     ''' <returns>Lista de registros de asistencia.</returns>
-    'Public Function GetAttendanceLogs() As List(Of AttendanceRecord)
-    '    Dim records As New List(Of AttendanceRecord)()
-
-    '    If IsConnected Then
-    '        Dim dwEnrollNumber As String = ""
-    '        Dim dwVerifyMode As Integer
-    '        Dim dwInOutMode As Integer
-    '        Dim dwYear As Integer
-    '        Dim dwMonth As Integer
-    '        Dim dwDay As Integer
-    '        Dim dwHour As Integer
-    '        Dim dwMinute As Integer
-    '        Dim dwSecond As Integer
-    '        Dim dwWorkMode As Integer
-
-    '        If Zkem.ReadGeneralLogData(DeviceNumber) Then
-    '            While Zkem.SSR_GetGeneralLogData(DeviceNumber, dwEnrollNumber, dwVerifyMode, dwInOutMode, dwYear, dwMonth, dwDay, dwHour, dwMinute, dwSecond, dwWorkMode)
-    '                Dim record As New AttendanceRecord With {
-    '                    .DeviceNumber = DeviceNumber,
-    '                    .EnrollNumber = dwEnrollNumber,
-    '                    .VerifyMode = dwVerifyMode,
-    '                    .InOutMode = dwInOutMode,
-    '                    .DateTime = New DateTime(dwYear, dwMonth, dwDay, dwHour, dwMinute, dwSecond),
-    '                    .WorkMode = dwWorkMode
-    '                }
-    '                records.Add(record)
-    '            End While
-    '        End If
-    '    End If
-
-    '    Return records
-    'End Function
     Public Function GetAttendanceLogs() As List(Of AttendanceRecord)
         Dim records As New List(Of AttendanceRecord)()
 

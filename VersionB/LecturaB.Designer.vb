4b548b362e1ed2202b85250c43ad0ae53eab1520
Dario Garcia <dario61081@gmail.com>
2024-12-11 22:23:59 -0300

    refactoring B

---
+++ VersionB/LecturaB.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LecturaB
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        StatusStrip1 = New StatusStrip()
+        ToolStripContainer1 = New ToolStripContainer()
+        SplitContainer1 = New SplitContainer()
+        SplitContainer2 = New SplitContainer()
+        ToolStripContainer1.ContentPanel.SuspendLayout()
+        ToolStripContainer1.SuspendLayout()
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).BeginInit()
+        SplitContainer1.Panel2.SuspendLayout()
+        SplitContainer1.SuspendLayout()
+        CType(SplitContainer2, ComponentModel.ISupportInitialize).BeginInit()
+        SplitContainer2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' StatusStrip1
+        ' 
+        StatusStrip1.Location = New Point(0, 327)
+        StatusStrip1.Name = "StatusStrip1"
+        StatusStrip1.Size = New Size(670, 22)
+        StatusStrip1.TabIndex = 0
+        StatusStrip1.Text = "StatusStrip1"
+        ' 
+        ' ToolStripContainer1
+        ' 
+        ' 
+        ' ToolStripContainer1.ContentPanel
+        ' 
+        ToolStripContainer1.ContentPanel.Controls.Add(SplitContainer1)
+        ToolStripContainer1.ContentPanel.Size = New Size(670, 302)
+        ToolStripContainer1.Dock = DockStyle.Fill
+        ToolStripContainer1.Location = New Point(0, 0)
+        ToolStripContainer1.Name = "ToolStripContainer1"
+        ToolStripContainer1.Size = New Size(670, 327)
+        ToolStripContainer1.TabIndex = 1
+        ToolStripContainer1.Text = "ToolStripContainer1"
+        ' 
+        ' SplitContainer1
+        ' 
+        SplitContainer1.Dock = DockStyle.Fill
+        SplitContainer1.Location = New Point(0, 0)
+        SplitContainer1.Name = "SplitContainer1"
+        ' 
+        ' SplitContainer1.Panel2
+        ' 
+        SplitContainer1.Panel2.Controls.Add(SplitContainer2)
+        SplitContainer1.Size = New Size(670, 302)
+        SplitContainer1.SplitterDistance = 223
+        SplitContainer1.TabIndex = 0
+        ' 
+        ' SplitContainer2
+        ' 
+        SplitContainer2.Dock = DockStyle.Fill
+        SplitContainer2.Location = New Point(0, 0)
+        SplitContainer2.Name = "SplitContainer2"
+        SplitContainer2.Size = New Size(443, 302)
+        SplitContainer2.SplitterDistance = 147
+        SplitContainer2.TabIndex = 0
+        ' 
+        ' LecturaB
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(670, 349)
+        Controls.Add(ToolStripContainer1)
+        Controls.Add(StatusStrip1)
+        Name = "LecturaB"
+        Text = "LecturaB"
+        ToolStripContainer1.ContentPanel.ResumeLayout(False)
+        ToolStripContainer1.ResumeLayout(False)
+        ToolStripContainer1.PerformLayout()
+        SplitContainer1.Panel2.ResumeLayout(False)
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainer1.ResumeLayout(False)
+        CType(SplitContainer2, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainer2.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripContainer1 As ToolStripContainer
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents SplitContainer2 As SplitContainer
+End Class

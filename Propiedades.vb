2a108643dfe68063cf5a7aadb50ec65e31719a45
FASTRONIC SERVER <FASTRONIC SERVER@FASTRONIC>
2025-01-13 14:24:45 -0300

    refactoring

--- Propiedades.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Propiedades
-    Private Sub BtnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
-        DialogResult = DialogResult.OK
-        Close()
-    End Sub
-End Class
